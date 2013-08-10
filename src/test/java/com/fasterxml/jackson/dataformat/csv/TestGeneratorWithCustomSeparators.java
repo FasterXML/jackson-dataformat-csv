23e74fbfc702041d6c92e969a27832a51b34e88f
Tatu Saloranta <tatu.saloranta@iki.fi>
2013-08-10 14:18:33 -0700

    also ensure that TSV writing should work

--- src/test/java/com/fasterxml/jackson/dataformat/csv/TestGeneratorWithCustomSeparators.java
+++ src/test/java/com/fasterxml/jackson/dataformat/csv/TestGeneratorWithCustomSeparators.java
@@ -11,4 +11,13 @@ public class TestGeneratorWithCustomSeparators extends ModuleTestBase
         String result = mapper.writer(schema).writeValueAsString(user);
         assertEquals("Barbie;Benton;FEMALE;false;\n", result);
     }
+
+    public void testTSV() throws Exception
+    {
+        CsvMapper mapper = mapperForCsv();
+        CsvSchema schema = mapper.schemaFor(FiveMinuteUser.class).withoutHeader().withColumnSeparator('\t');
+        FiveMinuteUser user = new FiveMinuteUser("Barbie", "Benton", false, FiveMinuteUser.Gender.FEMALE, null);
+        String result = mapper.writer(schema).writeValueAsString(user);
+        assertEquals("Barbie\tBenton\tFEMALE\tfalse\t\n", result);
+    }
 }
