d85789383d7faf2cb1bc6419d0863c28100356b0
Tyler Band <tylrm007@gmail.com>
2015-04-14 12:56:12 -0400

    added UPSERT (MERGE_INSERT) and test

--- csvparserMySQL.sql
+++ csvparserMySQL.sql
@@ -65,7 +65,7 @@ CREATE TABLE `mastercolumnmapper` (
   KEY `fk_MasterColumnMapper_ValidDataTypes1_idx` (`AttrDataType`),
   CONSTRAINT `fk_MasterColumnMapper_MasterMapper1` FOREIGN KEY (`idMasterMapper`) REFERENCES `mastermapper` (`idMasterMapper`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_MasterColumnMapper_ValidDataTypes1` FOREIGN KEY (`AttrDataType`) REFERENCES `validdatatypes` (`validDataTypes`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=309 DEFAULT CHARSET=utf8 COMMENT='numer';
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8 COMMENT='numer';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,7 +92,7 @@ CREATE TABLE `masterheader` (
   PRIMARY KEY (`masterHeaderID`),
   KEY `fk_MasterHeader_MasterMapper1_idx` (`idMasterMapper`),
   CONSTRAINT `fk_MasterHeader_MasterMapper1` FOREIGN KEY (`idMasterMapper`) REFERENCES `mastermapper` (`idMasterMapper`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='				';
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8 COMMENT='				';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -109,22 +109,33 @@ DROP TABLE IF EXISTS `mastermapper`;
 CREATE TABLE `mastermapper` (
   `idMasterMapper` mediumint(11) NOT NULL AUTO_INCREMENT,
   `MapName` varchar(45) NOT NULL,
-  `TargetTable` varchar(255) NOT NULL,
+  `TargetTable` varchar(255) DEFAULT NULL,
   `MapperDesc` text,
-  `VendorID` mediumint(11) DEFAULT NULL,
+  `AltForeignKey` mediumint(11) DEFAULT NULL,
+  `ApiKey` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idMasterMapper`),
-  KEY `fk_MasterMapper_vendor1_idx` (`VendorID`),
-  CONSTRAINT `fk_MasterMapper_vendor1` FOREIGN KEY (`VendorID`) REFERENCES `vendor` (`idvendor`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  KEY `fk_MasterMapper_vendor1_idx` (`AltForeignKey`),
+  CONSTRAINT `fk_MasterMapper_vendor1` FOREIGN KEY (`AltForeignKey`) REFERENCES `vendor` (`idvendor`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `mastermapper`
 --
 
-LOCK TABLES `mastermapper` WRITE;
-/*!40000 ALTER TABLE `mastermapper` DISABLE KEYS */;
-INSERT INTO `mastermapper` VALUES (1,'Inventory FBA','http://localhost:8080/KahunaService/rest/el-local/data1/v1/main:inventoryfba','Amazon FBA Inventory file',1),(2,'Inventory Monthly','Inventory',NULL,1),(3,'Inventory Health','http://localhost:8080/KahunaService/rest/el-local/data1/v1/main:inventory','this is the big one',NULL),(4,'ScanPower','http://localhost:8080/KahunaService/rest/el-local/data1/v1/main%3Ascanpower',NULL,NULL);
+DROP TABLE IF EXISTS `test_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test_table` (
+  `keycol` varchar(50) NOT NULL,
+  `numbercol` int(11) DEFAULT NULL,
+  `moneycol` decimal(10,2) DEFAULT NULL,
+  `datecol` date DEFAULT NULL,
+  `boolcol` bit(1) DEFAULT NULL,
+  `coltext` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`keycol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40000 ALTER TABLE `mastermapper` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -137,6 +148,9 @@ DROP TABLE IF EXISTS `validdatatypes`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `validdatatypes` (
   `validDataTypes` varchar(15) NOT NULL,
+  `regex` text,
+  `evalExpression` text,
+  `Comments` text,
   PRIMARY KEY (`validDataTypes`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -147,7 +161,10 @@ CREATE TABLE `validdatatypes` (
 
 LOCK TABLES `validdatatypes` WRITE;
 /*!40000 ALTER TABLE `validdatatypes` DISABLE KEYS */;
-INSERT INTO `validdatatypes` VALUES ('date'),('MMddYYY'),('money'),('number'),('text'),('yyyyMMdd');
+INSERT INTO `validdatatypes` VALUES ('boolean',NULL,NULL,NULL),('custom','','return value;',NULL),('date',NULL,NULL,NULL),('MM-dd-YYYY','',NULL,NULL),('money','/[$,()]/g','(value.charAt(0) == \'(\'? -1 : 1)*parseFloat(value.replace(regex,\'\'));',NULL),('number','/[$,()]/g','(value.charAt(0) == \'(\'? -1 : 1)*Number(value.replace(regex,\'\'));',NULL),('text',NULL,NULL,'this is the default - nothing to do here ');
+/*!40000 ALTER TABLE `validdatatypes` ENABLE KEYS */;
+UNLOCK TABLES;
+
 /*!40000 ALTER TABLE `validdatatypes` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
