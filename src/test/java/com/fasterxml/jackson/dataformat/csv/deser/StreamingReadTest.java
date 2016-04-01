f8e4784cb20b80188a524a94fab87551cd5b3e05
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-03-31 22:52:36 -0700

    remove unused methods

--- src/test/java/com/fasterxml/jackson/dataformat/csv/deser/StreamingReadTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/csv/deser/StreamingReadTest.java
@@ -11,36 +11,132 @@ import com.fasterxml.jackson.dataformat.csv.*;
  */
 public class StreamingReadTest extends ModuleTestBase
 {
-    public void testNumbers() throws Exception
-    {
-        CsvFactory factory = new CsvFactory();
-        String CSV = "111,16.25,-123456789\n";
+    private final CsvFactory CSV_F = new CsvFactory();
 
-        CsvSchema schema = CsvSchema.builder()
-                .addColumn("a")
-                .addColumn("b")
-                .addColumn("c")
-                .setUseHeader(false)
-                .build();
-        CsvParser parser = factory.createParser(CSV);
-        parser.setSchema(schema);
+    private final CsvSchema ABC_SCHEMA = CsvSchema.builder()
+            .addColumn("a")
+            .addColumn("b")
+            .addColumn("c")
+            .setUseHeader(false)
+            .build();
+
+    public void testIntRead() throws Exception
+    {
+        _testInts(1, 59, -8);
+        _testInts(10093525, -123456789, 123456789);
+        _testInts(-123451, 0, -829);
+        _testInts(Integer.MAX_VALUE, Integer.MIN_VALUE, 3);
+    }
+
+    public void testLongRead() throws Exception
+    {
+        _testLongs(1L, -3L);
+        _testLongs(1234567890L, -9876543212345679L);
+        _testLongs(Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public void testFloatRead() throws Exception
+    {
+        _testDoubles(1.0, 125.375, -900.5);
+        _testDoubles(10093525.125, -123456789.5, 123456789.0);
+        _testDoubles(-123451.75, 0.0625, -829.5);
+    }
+
+    private void _testInts(int a, int b, int c) throws Exception {
+        _testInts(false, a, b, c);
+        _testInts(true, a, b, c);
+    }
+
+    private void _testLongs(long a, long b) throws Exception {
+        _testLongs(false, a, b);
+        _testLongs(true, a, b);
+    }
+    
+    private void _testDoubles(double a, double b, double c) throws Exception {
+        _testDoubles(false, a, b, c);
+        _testDoubles(true, a, b, c);
+    }
+
+    private void _testInts(boolean useBytes, int a, int b, int c) throws Exception {
+        String CSV = String.format("%d,%d,%d\n", a, b, c);
+        CsvParser parser = useBytes ? CSV_F.createParser(CSV.getBytes("UTF-8"))
+                : CSV_F.createParser(CSV);
+        parser.setSchema(ABC_SCHEMA);
 
         assertToken(JsonToken.START_OBJECT, parser.nextToken());
 
         assertToken(JsonToken.FIELD_NAME, parser.nextToken());
         assertEquals("a", parser.getCurrentName());
-        assertEquals("111", parser.nextTextValue());
-        assertEquals(111L, parser.getLongValue());
+        assertEquals(""+a, parser.nextTextValue());
+        assertEquals(a, parser.getIntValue());
+        assertEquals((long) a, parser.getLongValue());
 
         assertEquals("b", parser.nextFieldName());
-
-        assertEquals("16.25", parser.nextTextValue());
-        assertEquals(16.25, parser.getDoubleValue());
+        assertEquals(""+b, parser.nextTextValue());
+        assertEquals((long) b, parser.getLongValue());
+        assertEquals(b, parser.getIntValue());
 
         assertTrue(parser.nextFieldName(new SerializedString("c")));
 
         assertToken(JsonToken.VALUE_STRING, parser.nextToken());
-        assertEquals(-123456789, parser.getIntValue());
+        assertEquals(c, parser.getIntValue());
+        assertEquals((long) c, parser.getLongValue());
+
+        assertToken(JsonToken.END_OBJECT, parser.nextToken());
+        assertNull(parser.nextToken());
+        
+        parser.close();
+    }
+
+    private void _testLongs(boolean useBytes, long a, long b) throws Exception {
+        String CSV = String.format("%d,%d\n", a, b);
+        CsvParser parser = useBytes ? CSV_F.createParser(CSV.getBytes("UTF-8"))
+                : CSV_F.createParser(CSV);
+        parser.setSchema(ABC_SCHEMA);
+
+        assertToken(JsonToken.START_OBJECT, parser.nextToken());
+
+        assertToken(JsonToken.FIELD_NAME, parser.nextToken());
+        assertEquals("a", parser.getCurrentName());
+        assertEquals(""+a, parser.nextTextValue());
+        assertEquals(a, parser.getLongValue());
+
+        assertEquals("b", parser.nextFieldName());
+        assertEquals(""+b, parser.nextTextValue());
+        assertEquals(b, parser.getLongValue());
+
+        assertToken(JsonToken.END_OBJECT, parser.nextToken());
+        assertNull(parser.nextToken());
+        
+        parser.close();
+    }
+    
+    private void _testDoubles(boolean useBytes, double a, double b, double c)
+            throws Exception
+    {
+        String CSV = String.format("%s,%s,%s\n", a, b, c);
+        CsvParser parser = useBytes ? CSV_F.createParser(CSV.getBytes("UTF-8"))
+                : CSV_F.createParser(CSV);
+        parser.setSchema(ABC_SCHEMA);
+
+        assertToken(JsonToken.START_OBJECT, parser.nextToken());
+
+        assertToken(JsonToken.FIELD_NAME, parser.nextToken());
+        assertEquals("a", parser.getCurrentName());
+        assertEquals(""+a, parser.nextTextValue());
+        assertEquals(a, parser.getDoubleValue());
+        assertEquals((float) a, parser.getFloatValue());
+
+        assertEquals("b", parser.nextFieldName());
+        assertEquals(""+b, parser.nextTextValue());
+        assertEquals((float) b, parser.getFloatValue());
+        assertEquals(b, parser.getDoubleValue());
+
+        assertTrue(parser.nextFieldName(new SerializedString("c")));
+
+        assertToken(JsonToken.VALUE_STRING, parser.nextToken());
+        assertEquals(c, parser.getDoubleValue());
+        assertEquals((float) c, parser.getFloatValue());
 
         assertToken(JsonToken.END_OBJECT, parser.nextToken());
         assertNull(parser.nextToken());
